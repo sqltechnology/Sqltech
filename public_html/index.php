2d091a024190484971134ce62388e10690fd8fd0
sanekakf <sashasaltikov2007@gmail.com>
2024-05-16 22:26:07 +0300

    bugfix with beget

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,13 @@
-<? header("http://sql-tec.ru/main/") ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <? 
+    $title = "Sql-Technologies";
+    require("./templates/head.php")
+    ?>
+</head>
+<body>
+    <? require "./templates/header.php" ?>
+    <div>Привет мир!</div>
+</body>
+</html>
